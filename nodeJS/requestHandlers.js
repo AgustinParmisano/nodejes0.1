d711726a5d2c9f4850cfe2077ca4a95fd6a8ba01
agustinparmisano <agustinparmisano@gmail.com>
2014-11-18 16:30:43 -0300

    siguiendo la guia: http://www.nodebeginner.org/index-es.html llegado …

--- nodeJS/requestHandlers.js
+++ nodeJS/requestHandlers.js
@@ -1,9 +1,22 @@
-function iniciar() {
-  console.log("Manipulador de petición 'iniciar' ha sido llamado.");
+var exec = require("child_process").exec;
+
+function iniciar(response) {
+  console.log("Manipulador de petición 'iniciar' fue llamado.");
+
+  exec("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function (error, stdout, stderr) {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
-function subir() {
-  console.log("Manipulador de petición 'subir' ha sido llamado.");
+function subir(response) {
+  console.log("Manipulador de petición 'subir' fue llamado.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Subir");
+  response.end();
 }
 
 exports.iniciar = iniciar;
